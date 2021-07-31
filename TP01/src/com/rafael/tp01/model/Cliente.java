0f35917e1b70fff5653a6452e344047ce1186172
rafa_ <rafa_@DESKTOP-0OBJLR3>
2021-07-30 22:32:56 -0300

    Commit da versão do TP01

---
+++ TP01/src/com/rafael/tp01/model/Cliente.java
@@ -0,0 +1,39 @@
+package com.rafael.tp01.model;
+import java.util.ArrayList;
+
+public class Cliente {
+		
+	private String nome;
+	private String endereco;
+	private String telefone;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getEndereco() {
+		return endereco;
+	}
+	
+	public String getTelefone() {
+		return telefone;
+	}
+		
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	
+	
+	
+	
+
+}
